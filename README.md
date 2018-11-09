ced6292f63e68b107ef523b3151004426a1d2e3f
msbundles <25093511+msbundles@users.noreply.github.com>
2018-11-08 23:00:34 -0600

    adding first part of p5 project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Bundles Personal Programming Betterment
+This is a repository for me to do things in order to practice Programming
---
+++ p5js/1/sketch.js
@@ -0,0 +1,7 @@
+function setup (){
+  createCanvas(windowWidth, windowHeight);
+  ellipse(850,250,100,100)
+}
+function windowResized(){
+	     resizeCanvas(windowWidth, windowHeight);
+}
