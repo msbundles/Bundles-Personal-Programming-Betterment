faa719fb632c6503327ae573623fac15a11e884e
msbundles <25093511+msbundles@users.noreply.github.com>
2018-11-14 22:46:23 -0600

    Fixing firmatting and adding clearing function on backpace key.

--- p5js/1-Dots/sketch.js
+++ p5js/1-Dots/sketch.js
@@ -1,34 +1,40 @@
 var x = 750;
 var y = 360;
 var scalee = 10;
-function setup (){
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-function windowResized(){
-	     resizeCanvas(windowWidth, windowHeight);
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
-  function keyPressed(){
-    if (keyCode === UP_ARROW) {
-      y -= scalee
-    }
-  else if (keyCode === DOWN_ARROW) {
-      y += scalee
-    }
-  else if (keyCode === LEFT_ARROW) {
-      x -= scalee
-    }
-  else if (keyCode === RIGHT_ARROW) {
-      x += scalee
-    }
-//W key ti increment distance
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    y -= scalee
+  } else if (keyCode === DOWN_ARROW) {
+    y += scalee
+  } else if (keyCode === LEFT_ARROW) {
+    x -= scalee
+  } else if (keyCode === RIGHT_ARROW) {
+    x += scalee
+  }
+  //W key to increment distance
   else if (keyCode === 87) {
-        scalee += 10;
-      }
-//S key to decrment distance
+    scalee += 10;
+  }
+  //S key to decrment distance
   else if (keyCode === 83) {
-          scalee -= 10;
-      }
+    scalee -= 10;
+  } else if (keyCode === 8) {
+    background(0)
   }
-  function draw(){
-    ellipse(x,y,100,100);
-  }
+}
+if (keyCode === 8) {
+  background(255)
+}
+
+function draw() {
+  ellipse(x, y, 100, 100);
+}
