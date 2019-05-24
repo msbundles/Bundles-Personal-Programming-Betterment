b08513ae215b9294f99f3d69fa30ce112c039af6
Aidan <25093511+msbundles@users.noreply.github.com>
2019-05-24 11:48:03 -0500

    Adding comments

--- docs/p5/1-Dots/sketch.js
+++ docs/p5/1-Dots/sketch.js
@@ -1,8 +1,13 @@
+//inital start location for the circle and variables that dictate circle position.
 var x = 750;
 var y = 360;
-var scalee = 10;
+//scale var for distance between dots
+var distB = 10;
+//Count variable for switch statment
 var count = 0;
+//The size of the circle being drawn
 var circlSiz = 100;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -12,26 +17,27 @@ function windowResized() {
 }
 
 function keyPressed() {
+  //logic block for changing circle position
   if (keyCode === UP_ARROW) {
-    y -= scalee;
+    y -= distB;
   } else if (keyCode === DOWN_ARROW) {
-    y += scalee;
+    y += distB;
   } else if (keyCode === LEFT_ARROW) {
-    x -= scalee;
+    x -= distB;
   } else if (keyCode === RIGHT_ARROW) {
-    x += scalee;
+    x += distB;
   } else if (keyCode === 16) {
-    //shift key to re sed position
+    //shift key to reset circle position
     x = 750;
     y = 360;
   } else if (keyCode === 87) {
     //W key to increment distance
-    scalee += 10;
-    console.log(`the distance between dots = ${scalee}`);
+    distB += 10;
+    console.log(`the distance between dots = ${distB}`);
   } else if (keyCode === 83) {
     //S key to decrement distance
-    scalee -= 10;
-    console.log(`the distance between dots = ${scalee}`);
+    distB -= 10;
+    console.log(`the distance between dots = ${distB}`);
   } else if (keyCode === 8) {
     //backspace to clear
     background(255);
@@ -41,19 +47,24 @@ function keyPressed() {
     x = 750;
     y = 360;
   } else if (keyCode === 50) {
+    //Logic for changing color switch statment(2 for +,1 for -)
     count++;
   } else if (keyCode === 49) {
     count--;
   } else if (keyCode === 90) {
+    //Logic block for changing circle size(z for +, x for -)
     circlSiz++;
   } else if (keyCode === 88) {
     circlSiz--;
   } else if (keyCode === 67) {
+    //c key for size reset
     circlSiz = 100;
   }
 }
 function draw() {
+  //drawing circles
   ellipse(x, y, circlSiz, circlSiz);
+  //switch for changing color
   switch (count) {
     case 0:
       fill(255, 255, 255);
@@ -67,8 +78,12 @@ function draw() {
     case 3:
       fill(255, 110, 0);
       break;
-    default:
-      fill(255, 255, 255);
-      break;
+  }
+  //logic for keeping switch in range
+  if (count == -1) {
+    count = 0;
+  }
+  if (count > 3) {
+    count = 1;
   }
 }
